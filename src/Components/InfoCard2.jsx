452bd97517ba8201241d78a1c34cba652ca7c302
André <103223053+AndreFernandoM@users.noreply.github.com>
2022-11-16 13:12:44 -0300

    update: Components Style

--- src/Components/InfoCard2.jsx
+++ src/Components/InfoCard2.jsx
@@ -7,12 +7,9 @@ import CardImg from './CardImg.jsx';
 const InfoCard2 = (props) => {
 	return (
 		<div className="infoCard2">
-			<div classNameName="inforCardDescriptiondDiv">
-				<p className="inforCardDescriptionDivP">
-					{props.children}
-				</p>
-			</div>
-
+			<p className="inforCardDescription">
+				{props.children}
+			</p>
 			<CardImg type={props.type} link={props.img} size={props.size}/>
 		</div>
 	)
