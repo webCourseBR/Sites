ff6b94bb7506982973e5cdae2e260d967cf46d5c
Felipe Pegoraro <felipe.pegoraro@sou.unaerp.edu.br>
2022-11-16 22:11:14 -0300

    edit/add: Home, animations, Dropdown, some responsive css (...)

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../style/scss/Header.scss';
 
-// TODO:
-// - EDITAR CSS
-// - EXPANDIR UL BOTAO
+import Dropdown from 'react-bootstrap/Dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 	return (
-		<header className="header">
+		<header className="header shadow-sm">
 
 			<div className="headerLeftDiv">
 				<img className="headerLeftLogo" src="Assets/images/logo-header-cat.png" alt="logo da pagina"/>
@@ -17,13 +17,32 @@ const Header = () => {
 			<div className="headerRightDiv">
 				<button className='buttonAdoptMe'>ME ADOTE!</button>
 
-				<a href="...">
+				<Link to="/" className="headerLogoToHome">
 					<img src="Assets/images/logo-header-house.png" alt="editar isso aqui"/>
-				</a>
+				</Link>
 
-				<a href="...">
+
+			<Dropdown className="buttonDropdown">
+				<Dropdown.Toggle id="dropdown-basic" className="buttonDropdownToggle">
 					<img src="Assets/images/header-ex.png" alt="editar isso aqui"/>
-				</a>
+				</Dropdown.Toggle>
+				<Dropdown.Menu className="buttonDropdownMenu">
+					<Dropdown.Item>
+						<Link to="/"><p className="buttonDropdownP">HOME</p></Link>
+					</Dropdown.Item>
+					<Dropdown.Item>
+						<Link to="/cats"><p className="buttonDropdownP">CATS</p></Link>
+					</Dropdown.Item>
+					<Dropdown.Item>
+						<Link to="/dogs"><p className="buttonDropdownP">DOGS</p></Link>
+					</Dropdown.Item>
+					<Dropdown.Item>
+						<Link to="/donates"><p className="buttonDropdownP">DONATES</p></Link>
+					</Dropdown.Item>
+				</Dropdown.Menu>
+			</Dropdown>
+
+	
 			</div>
 		</header>
 	)
