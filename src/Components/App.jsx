04859c91c4266f640c6f7967aa6a2311f57e0bd0
pbcz1k4 <pedroborgespedro176@gmail.com>
2022-11-19 17:48:49 -0300

    add: page License

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -7,6 +7,7 @@ import Home from '../Pages/Home.jsx';
 import Dogs from '../Pages/Dogs.jsx';
 import Cats from '../Pages/Cats.jsx';
 import Donate from '../Pages/Donate';
+import License from '../Pages/License.jsx';
 
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
@@ -26,6 +27,7 @@ function App() {
 					<Route path="/cats" component={Cats}/>
 					<Route path="/dogs" component={Dogs}/>
 					<Route path="/donate" component={Donate}/>
+					<Route path="/license" component={License}/>
 				</main>
 				<Footer />
 			</div>
